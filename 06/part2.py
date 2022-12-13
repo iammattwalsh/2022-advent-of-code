21844d1cc1224c008b638cec6f8df533cfce83dc
MattDWalsh <matthew.david.walsh@gmail.com>
2022-12-13 10:22:26 -0800

    completed day 6 part 2

--- 06/part2.py
+++ 06/part2.py
@@ -1,14 +1,14 @@
 with open('input.txt', 'r') as f:
     msg = ''.join(f.readlines())
 
-for i in range(len(msg) - 4):
+for i in range(len(msg) - 14):
     counter = 0
-    code = msg[i : i + 4]
+    code = msg[i : i + 14]
     for char in code:
         if code.count(char) == 1:
             counter += 1
         else:
             break
-    if counter == 4:
-        print(i + 4)
+    if counter == 14:
+        print(i + 14)
         break
