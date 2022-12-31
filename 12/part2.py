0483f642a4be14d32a0bbf81129887fadb55f5ce
MattDWalsh <matthew.david.walsh@gmail.com>
2022-12-31 10:18:49 -0800

    completed day 12 part 2

--- 12/part2.py
+++ 12/part2.py
@@ -4,7 +4,9 @@ class Grid:
     def __init__(self, input):
         self.tiles = []
         self.grid = {}
-
+        self.lowest = []
+        self.shortest = None
+        
         for y, row in enumerate(input):
             for x, char in enumerate(row):
                 adj = []
@@ -17,16 +19,22 @@ class Grid:
                 if x != len(row) - 1 and self.height(input[y][x + 1]) - self.height(char) <= 1:
                     adj.append((x + 1, y))
                 id = (x, y)
-                if char == 'S':
-                    self.start = id
-                elif char == 'E':
+                if x == 0 and self.height(char) == 0:
+                    self.lowest.append(id)
+                if char == 'E':
                     self.end = id
                 self.tiles.append(id)
                 self.grid[id] = adj
 
-    def dijkstra(self):
+    def loop_starts(self):
+        for tile in self.lowest:
+            path = self.dijkstra(tile)
+            if self.shortest == None or path[self.end] < self.shortest[self.end]:
+                self.shortest = path
+
+    def dijkstra(self, start):
         unvisited = {tile: float('inf') for tile in self.tiles}
-        unvisited[self.start] = 0
+        unvisited[start] = 0
         visited = {}
         parents = {}
         while unvisited:
@@ -52,5 +60,5 @@ if __name__ == '__main__':
     with open('input.txt', 'r') as f:
         input = [[char for char in line.strip('\n')] for line in f.readlines()]
     grid = Grid(input)
-    path = grid.dijkstra()
-    print(path[grid.end])
+    grid.loop_starts()
+    print(grid.shortest[grid.end])
